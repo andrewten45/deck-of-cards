d71ed55fde45dd8ccc97a84e7be73b3facb69852
Andrew Tennant <andrewtennant2012@gmail.com>
2021-11-15 15:20:56 -0600

    Update ruby.rb

--- ruby.rb
+++ ruby.rb
@@ -1,3 +1,14 @@
 # First object for deck of cards app
 
-gem 'rubycards'
+require 'rubygems'
+require 'rubycards'
+include rubycards
+
+hand = Hand.new
+deck = Deck.new
+
+deck.shuffle!
+
+hand.draw(deck, 5)
+
+puts hand
